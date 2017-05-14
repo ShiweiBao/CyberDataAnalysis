7d18886cd732a4484ca4dafb407f6b92a7999323
zyclarkcheng <zyclark.cheng@gmail.com>
2017-05-14 10:06:14 +0200

    Add files via upload

--- lab1/codes/nn.py
+++ lab1/codes/nn.py
@@ -1,120 +1,92 @@
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
-import numpy as np
-
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-from tensorflow import nn
+import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.cross_validation import train_test_split
 
-# some useful functions...
-def weight_variable(shape):
-    """weight_variable generates a weight variable of a given shape."""
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+# Generate tain and test data
+X, Y = make_classification(n_samples=50000, n_features=10, n_informative=8, 
+                    n_redundant=0, n_clusters_per_class=2)
+Y = np.array([Y, -(Y-1)]).T  # The model currently needs one column for each class
+X, X_test, Y, Y_test = train_test_split(X, Y)
 
-def bias_variable(shape):
-    """bias_variable generates a bias variable of a given shape."""
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+# Parameters
+learning_rate = 0.001
+training_epochs = 10
+batch_size = 100
+display_step = 1
 
-# load data
-# with one_hot=True, categorical labels (e.g. {1,2,3...}) coded into binary vector form (e.g.{[1,0,0],[0,1,0]...})
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+# Network Parameters
+n_hidden_1 = 10 # 1st layer number of features
+n_hidden_2 = 10 # 2nd layer number of features
+n_input = 10 # Number of feature
+n_classes = 2 # Number of classes to predict
 
-# visual check for the loaded dataset
-disp_target = 4
-plt.imshow(mnist.train.images[disp_target].reshape((28,28)),cmap='gray_r')
-plt.title(
-    'Train Observation {:d} - Label : {:d}'.format(
-        disp_target,
-        np.argmax(mnist.train.labels[disp_target])
-    )
-)
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
 
-    
-# x = input / y = label
-# first dimension of place holder set as None. With this first dimension can be dynamic size per batch
-x = tf.placeholder(tf.float32, shape=[None,28*28])
-y = tf.placeholder(tf.float32, shape=[None,10])
+# Create model
+def multilayer_perceptron(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
 
-# setup parameters
-W1 = weight_variable([28*28,100])
-b1 = bias_variable([100])
+    # Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
 
-W2 = weight_variable([100,100])
-b2 = bias_variable([100])
+# Construct model
+pred = multilayer_perceptron(x, weights, biases)
 
-W3 = weight_variable([100,100])
-b3 = bias_variable([100])
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-Wo = weight_variable([100,10])
-bo = bias_variable([10])
+# Initializing the variables
+init = tf.initialize_all_variables()
 
-# make a inference expression w/ Variables
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(len(X)/batch_size)
+        X_batches = np.array_split(X, total_batch)
+        Y_batches = np.array_split(Y, total_batch)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_x, batch_y = X_batches[i], Y_batches[i]
+            # batch_y.shape = (batch_y.shape[0], 1)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                          y: batch_y})
+            # Compute average loss
+            avg_cost += c / total_batch
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print "Epoch:", '%04d' % (epoch+1), "cost=", \
+                "{:.9f}".format(avg_cost)
+    print "Optimization Finished!"
 
-# 3 hidden layers
-h1 = nn.relu(tf.matmul(x,W1) + b1)
-h2 = nn.relu(tf.matmul(h1,W2) + b2)
-h3 = nn.relu(tf.matmul(h2,W3) + b3)
-
-# read out layer
-o = tf.matmul(h3,Wo) + bo
-
-# set cross_entropy loss function with y (target) and o (logits)
-cross_entropy = tf.reduce_mean(
-    nn.softmax_cross_entropy_with_logits(
-        labels=y,
-        logits=o
-    )
-)
-
-# then we'll get update rule (train op) here
-train_step = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cross_entropy)
-
-# binary accuracy calculation
-correct_prediction = tf.equal(tf.argmax(o,1), tf.argmax(y,1))
-# ...and its mean per feed
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-m = 100 # batch size
-n = 1000 # number of batches to train
-acc_tr = []
-acc_vl = []
-
-# Assume that you have 2GB of GPU memory and want to allocate 512MB:
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
-
-# open an session
-with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-    
-    # Initialize Variables
-    sess.run(tf.global_variables_initializer())
-
-    for j in xrange(1000):
-        # here we fetch a batch from dataset
-        batch = mnist.train.next_batch(m)
-        # update is conducted by tf
-        # feed dict is key-value dictionary which
-        # each keys are placeholder and
-        # each values are actual value for placeholders
-        sess.run(train_step,feed_dict={x:batch[0], y:batch[1]})
-        
-        if j%100==0:
-            acc_tr.append(accuracy.eval(feed_dict={x:batch[0], y:batch[1]}))
-            acc_vl.append(accuracy.eval(feed_dict={x:mnist.test.images, y:mnist.test.labels}))
-
-            print(
-                '{:d}th Iteration - Train Acc: {:.2%} - Test Acc: {:.2%}'.format(
-                    j,acc_tr[-1],acc_vl[-1]
-                )
-            )
-
-
-fig,ax = plt.subplots()
-ax.plot(acc_tr, label='Training Accuracy')
-ax.plot(acc_vl, label='Validation Accuracy')
-ax.grid(True)
-ax.set_ylabel('Accuracy')
-ax.set_xlabel('Iteration')
-ax.set_title('Training Result')
-ax.legend()
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print "Accuracy:", accuracy.eval({x: X_test, y: Y_test})
+    global result 
+    result = tf.argmax(pred, 1).eval({x: X_test, y: Y_test})
