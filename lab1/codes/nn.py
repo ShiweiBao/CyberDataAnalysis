c53d944105b652c1d989f794c2e4f5b5357b600b
zyclarkcheng <zyclark.cheng@gmail.com>
2017-05-14 09:56:46 +0200

    Add files via upload

--- lab1/codes/nn.py
+++ lab1/codes/nn.py
@@ -1,113 +1,120 @@
-import tensorflow as tf
-
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import numpy as np
 
-import melt_dataset
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow import nn
 
-import sys
+# some useful functions...
+def weight_variable(shape):
+    """weight_variable generates a weight variable of a given shape."""
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
-from sklearn.metrics import roc_auc_score
+def bias_variable(shape):
+    """bias_variable generates a bias variable of a given shape."""
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
-   
+# load data
+# with one_hot=True, categorical labels (e.g. {1,2,3...}) coded into binary vector form (e.g.{[1,0,0],[0,1,0]...})
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-def init_weights(shape):
+# visual check for the loaded dataset
+disp_target = 4
+plt.imshow(mnist.train.images[disp_target].reshape((28,28)),cmap='gray_r')
+plt.title(
+    'Train Observation {:d} - Label : {:d}'.format(
+        disp_target,
+        np.argmax(mnist.train.labels[disp_target])
+    )
+)
 
-    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+    
+# x = input / y = label
+# first dimension of place holder set as None. With this first dimension can be dynamic size per batch
+x = tf.placeholder(tf.float32, shape=[None,28*28])
+y = tf.placeholder(tf.float32, shape=[None,10])
 
-   
+# setup parameters
+W1 = weight_variable([28*28,100])
+b1 = bias_variable([100])
 
-def model(X, w_h, w_o):
+W2 = weight_variable([100,100])
+b2 = bias_variable([100])
 
-    h = tf.nn.relu(tf.matmul(X, w_h)) # this is a basic mlp, think 2 stacked logistic regressions
+W3 = weight_variable([100,100])
+b3 = bias_variable([100])
 
-    return tf.matmul(h, w_o) # note that we dont take the softmax at the end because our cost fn does that for us
+Wo = weight_variable([100,10])
+bo = bias_variable([10])
 
-   
+# make a inference expression w/ Variables
 
-batch_size = 50
+# 3 hidden layers
+h1 = nn.relu(tf.matmul(x,W1) + b1)
+h2 = nn.relu(tf.matmul(h1,W2) + b2)
+h3 = nn.relu(tf.matmul(h2,W3) + b3)
 
-learning_rate = 0.1
+# read out layer
+o = tf.matmul(h3,Wo) + bo
 
-num_iters = 500
+# set cross_entropy loss function with y (target) and o (logits)
+cross_entropy = tf.reduce_mean(
+    nn.softmax_cross_entropy_with_logits(
+        labels=y,
+        logits=o
+    )
+)
 
-hidden_size = 20
+# then we'll get update rule (train op) here
+train_step = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cross_entropy)
 
-   
+# binary accuracy calculation
+correct_prediction = tf.equal(tf.argmax(o,1), tf.argmax(y,1))
+# ...and its mean per feed
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-argv = sys.argv
+m = 100 # batch size
+n = 1000 # number of batches to train
+acc_tr = []
+acc_vl = []
 
-trainset = argv[1]
+# Assume that you have 2GB of GPU memory and want to allocate 512MB:
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
 
-testset = argv[2]
+# open an session
+with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+    
+    # Initialize Variables
+    sess.run(tf.global_variables_initializer())
 
-   
+    for j in xrange(1000):
+        # here we fetch a batch from dataset
+        batch = mnist.train.next_batch(m)
+        # update is conducted by tf
+        # feed dict is key-value dictionary which
+        # each keys are placeholder and
+        # each values are actual value for placeholders
+        sess.run(train_step,feed_dict={x:batch[0], y:batch[1]})
+        
+        if j%100==0:
+            acc_tr.append(accuracy.eval(feed_dict={x:batch[0], y:batch[1]}))
+            acc_vl.append(accuracy.eval(feed_dict={x:mnist.test.images, y:mnist.test.labels}))
 
-trX, trY = melt_dataset.load_dense_data(trainset)
+            print(
+                '{:d}th Iteration - Train Acc: {:.2%} - Test Acc: {:.2%}'.format(
+                    j,acc_tr[-1],acc_vl[-1]
+                )
+            )
 
-print ("finish loading train set ",trainset)
 
-teX, teY = melt_dataset.load_dense_data(testset)
-
-print ("finish loading test set ", testset)
-
-   
-
-num_features = trX[0].shape[0]
-
-print ('num_features: ',num_features)
-
-print ('trainSet size: ', len(trX))
-
-print ('testSet size: ', len(teX))
-
-print ('batch_size:', batch_size, ' learning_rate:', learning_rate, ' num_iters:', num_iters)
-
-   
-
-X = tf.placeholder("float", [None, num_features]) # create symbolic variables
-
-Y = tf.placeholder("float", [None, 1])
-
-   
-
-w_h = init_weights([num_features, hidden_size]) # create symbolic variables
-
-w_o = init_weights([hidden_size, 1])
-
-   
-
-py_x = model(X, w_h, w_o)
-
-   
-
-cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(py_x, Y)) # compute costs
-
-train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost) # construct an optimizer
-
-predict_op = tf.nn.relu(py_x)
-
-   
-
-sess = tf.Session()
-
-init = tf.initialize_all_variables()
-
-sess.run(init)
-
-   
-
-for i in range(num_iters):
-
-    predicts, cost_ = sess.run([predict_op, cost], feed_dict={X: teX, Y: teY})
-
-    print (i, 'auc:', roc_auc_score(teY, predicts), 'cost:', cost_)
-
-for start, end in zip(range(0, len(trX), batch_size), range(batch_size, len(trX), batch_size)):
-
-    sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
-
-   
-
-predicts, cost_ = sess.run([predict_op, cost], feed_dict={X: teX, Y: teY})
-
-print ('final ', 'auc:', roc_auc_score(teY, predicts),'cost:', cost_)
+fig,ax = plt.subplots()
+ax.plot(acc_tr, label='Training Accuracy')
+ax.plot(acc_vl, label='Validation Accuracy')
+ax.grid(True)
+ax.set_ylabel('Accuracy')
+ax.set_xlabel('Iteration')
+ax.set_title('Training Result')
+ax.legend()
