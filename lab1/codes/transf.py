8ff60df7520dd0f81ed4d519864491f480b9acaa
zyclarkcheng <zyclark.cheng@gmail.com>
2017-05-16 11:55:56 +0200

    Add files via upload

--- lab1/codes/transf.py
+++ lab1/codes/transf.py
@@ -9,13 +9,13 @@ import pandas as pd
 import numpy as np
 import scipy.io as sio
 from collections import Counter
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import neighbors
 from sklearn import datasets, linear_model
 from sklearn import metrics
 from sklearn.metrics import roc_curve, auc,accuracy_score
-#from imblearn.over_sampling import     SMOTE
+from imblearn.over_sampling import     SMOTE
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 import numpy as np
@@ -26,56 +26,18 @@ from itertools import cycle
 from sklearn import svm, datasets
 from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import StratifiedKFold
+#
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import interp
+from sklearn.datasets import make_classification
+from sklearn.cross_validation import KFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve
 ##-----Let's do the classification-------##
 ##-----try 3 classifiers with both SMOTED and UNSMOTED data------##
 
 
-def roccrossvalid(dffeatures,labels,clfname):
-    # Run classifier with cross-validation and plot ROC curves
-    X = dffeatures
-    y = labels
-    # X, y = X[y != 2], y[y != 2]
-    n_samples, n_features = X.shape
-
-    cv = StratifiedKFold(n_splits=6)
-    classifier = neighbors.KNeighborsClassifier(n_neighbors=1)
-    # classifier = svm.SVC(kernel='linear', probability=True,
-    #                      random_state=random_state)
-
-    mean_tpr = 0.0
-    mean_fpr = np.linspace(0, 1, 100)
-
-    colors = cycle(['cyan', 'indigo', 'seagreen', 'yellow', 'blue', 'darkorange'])
-    lw = 2
-
-    i = 0
-    for (train, test), color in zip(cv.split(X, y), colors):
-        probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
-        # Compute ROC curve and area the curve
-        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
-        mean_tpr += interp(mean_fpr, fpr, tpr)
-        mean_tpr[0] = 0.0
-        roc_auc = auc(fpr, tpr)
-        plt.plot(fpr, tpr, lw=lw, color=color,
-                 label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
-
-        i += 1
-    plt.plot([0, 1], [0, 1], linestyle='--', lw=lw, color='k',
-             label='Luck')
-
-    mean_tpr /= cv.get_n_splits(X, y)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    plt.plot(mean_fpr, mean_tpr, color='g', linestyle='--',
-             label='Mean ROC (area = %0.2f)' % mean_auc, lw=lw)
-
-    plt.xlim([-0.05, 1.05])
-    plt.ylim([-0.05, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic for '+ clfname)
-    plt.legend(loc="lower right")
-    plt.show()
 def classify(dffeatures,labels,clfname):
     TP, FP, FN, TN = 0, 0, 0, 0
     x_array = np.array(dffeatures)
@@ -229,4 +191,52 @@ def transf(df):
     dfcs.loc[:,'cvcresponsecode']=pd.to_numeric(dfcs.loc[:,'cvcresponsecode'])
     
     return dfcs
+def roccrossvalid(dffeatures,labels,clfname):
+    # Run classifier with cross-validation and plot ROC curves
+    X = dffeatures.values
+    y = labels.values
+    # X, y = X[y != 2], y[y != 2]
+    n_samples, n_features = X.shape
 
+    cv = StratifiedKFold(n_splits=10)
+    # classifier = neighbors.KNeighborsClassifier(n_neighbors=1)
+    classifier = MLPClassifier(hidden_layer_sizes=(300,))
+    # classifier = svm.SVC(kernel='linear', probability=True,
+    #                      random_state=random_state)
+
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+
+    colors = cycle(['cyan', 'indigo', 'seagreen', 'yellow', 'blue', 'darkorange'])
+    lw = 2
+
+    i = 0
+    for (train, test), color in zip(cv.split(X, y), colors):
+        x_train, y_train = dosm(X[train], y[train])
+
+        probas_ = classifier.fit(x_train, y_train).predict_proba(X[test])
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, lw=lw, color=color,
+                 label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+
+        i += 1
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=lw, color='k',
+             label='baseline')
+
+    mean_tpr /= cv.get_n_splits(X, y)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    plt.plot(mean_fpr, mean_tpr, color='k', linestyle='-',
+             label='Mean ROC (area = %0.2f)' % mean_auc, lw=lw)
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic for '+ clfname)
+    plt.legend(loc="lower right")
+    plt.show()
